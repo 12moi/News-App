ff3aada5014e8198a4054cabac4dd6b8309b0420
12moi <12moi>
2022-03-17 00:35:46 +0300

    add remove spaces

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,4 @@
 
-
-
-
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
@@ -11,8 +8,6 @@ bootstrap =Bootstrap()
 # initializing application
 def create_app(config_name):
     app = Flask(__name__)
-
-
 
     # setting up configuration
     app.config.from_object(config_options[config_name])
