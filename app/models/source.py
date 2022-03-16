a12b1d6ab6885f81a4d5a715abcb3b776dbed363
12moi <12moi>
2022-03-17 01:06:15 +0300

    add updates to removal of folder

--- app/models/source.py
+++ app/models/source.py
@@ -3,6 +3,14 @@ class Source:
     '''
     Sources class that defines each source object
     '''
+
+
+
+
+
+
+
+    
     def __init__(self,id, name, description,url,category,language,country):
         self.id=id
         self.name=name
