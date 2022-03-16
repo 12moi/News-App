31e28e3a42dafe6cfbc9d099bc010a7b9f11e5dc
12moi <12moi>
2022-03-17 00:31:46 +0300

    remove space

--- tests/articles_test.py
+++ tests/articles_test.py
@@ -3,29 +3,6 @@
 import unittest
 from app.models import Article
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class ArticleTest(unittest.TestCase):
     '''
     Test class to test the behaviour of the Article class
